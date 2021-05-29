faef98981c85a78af84b83b56450e36fbfc14e41
Oscar Pacheco <Oscarpa9188@gmail.com>
2021-05-29 07:59:35 -0500

    Components / Axios

---
+++ src/components/ListComponent.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import axios from 'axios'
+
+class ListComponent extends Component {
+    
+    render(){
+        return(
+            <div>
+                List Component
+            </div>
+        )
+    }
+}
+
+export default ListComponent;
