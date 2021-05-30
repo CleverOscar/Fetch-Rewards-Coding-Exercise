18b2282ca303b06832f4089c0c8bb7670f96f147
Oscar Pacheco <Oscarpa9188@gmail.com>
2021-05-30 11:54:41 -0500

    Update UserData.jsx

--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 // import axios from 'axios';
 
 function UserData(props) {
-
+    
+    // filter out empty name properties such as null or ""
     const showData = props.data.filter(el => el.name !== null && el.name !== "");
 
-
-    console.log(showData.sort((a,b) => {
-        if(a.listId >= b.listId){
+    // sorting Data by listId
+    showData.sort((a,b) => {
+        if(a.listId > b.listId){
+            console.log(a.listId)
             return 1
         } else {
             return -1
         }
-    }))
+    })
     
     const userInfo = showData.map(el => <div>
          <tr key={el.id}>
