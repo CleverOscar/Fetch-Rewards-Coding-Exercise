2c7c7596921cb9f6dcc4bd63ed5ac8f290ae2378
Oscar Pacheco <Oscarpa9188@gmail.com>
2021-06-02 21:28:43 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,6 @@ import {
 
 import ListComponent from './components/ListComponent';
 
-
-/*
-Display all the items grouped by "listId"
-Sort the results first by "listId"
-then by "name"
-when displaying.
-Filter out any items where "name"
-is blank or null. 
-*/ 
-
 function App() {
   return (
     <div className="App">
